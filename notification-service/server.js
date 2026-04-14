31ec51cef2d17761781c1d01f05e85faca0a1201
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-14 14:07:38 +0300

    Main file for running notification-service

---
+++ notification-service/server.js
@@ -0,0 +1,20 @@
+const amqp = require("amqplib");
+
+async function start() {
+  const conn = await amqp.connect("amqp://rabbitmq");
+  const ch = await conn.createChannel();
+
+  await ch.assertQueue("orders");
+
+  console.log("Notification service started");
+
+  ch.consume("orders", (msg) => {
+    const data = JSON.parse(msg.content.toString());
+
+    console.log("📩 EVENT:", data);
+
+    ch.ack(msg);
+  });
+}
+
+start();
