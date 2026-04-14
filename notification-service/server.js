77e8d2d14ca925b12bfa67cb9c05e1eb30158449
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-14 14:11:35 +0300

    Updated notification-service

--- notification-service/server.js
+++ notification-service/server.js
@@ -4,14 +4,25 @@ async function start() {
   const conn = await amqp.connect("amqp://rabbitmq");
   const ch = await conn.createChannel();
 
-  await ch.assertQueue("orders");
+  await ch.assertQueue("events");
 
   console.log("Notification service started");
 
-  ch.consume("orders", (msg) => {
-    const data = JSON.parse(msg.content.toString());
+  ch.consume("events", (msg) => {
+    const event = JSON.parse(msg.content.toString());
 
-    console.log("EVENT:", data);
+    switch (event.type) {
+      case "order.created":
+        console.log(`Email: Order created for user ${event.userId}`);
+        break;
+
+      case "order.status":
+        console.log(`Email: Order ${event.orderId} status = ${event.status}`);
+        break;
+
+      default:
+        console.log("Unknown event", event);
+    }
 
     ch.ack(msg);
   });
