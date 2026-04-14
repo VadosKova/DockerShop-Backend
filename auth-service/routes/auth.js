72737e90e66f9fcc1a5732f0fcd2c52b6dd079d0
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-14 13:07:55 +0300

    Routes for register and login

---
+++ auth-service/routes/auth.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  const hashed = await bcrypt.hash(req.body.password, 10);
+
+  const user = await User.create({
+    ...req.body,
+    password: hashed
+  });
+
+  res.json(user);
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) return res.status(400).send("User not found");
+
+  const valid = await bcrypt.compare(req.body.password, user.password);
+
+  if (!valid) return res.status(400).send("Wrong password");
+
+  const token = jwt.sign(
+    { id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+});
+
+module.exports = router;
