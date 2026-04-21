e5e76c6caa893c05d1968d9c2c22efc06c8c0cc8
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-21 12:37:21 +0300

    Created admin

--- auth-service/server.js
+++ auth-service/server.js
@@ -7,8 +7,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+const User = require("./models/User");
+const bcrypt = require("bcrypt");
 
+mongoose.connect(process.env.MONGO_URI).then(async () => {
+  const adminExists = await User.findOne({ email: "admin@gmail.com" });
+  if (!adminExists) {
+    const hashed = await bcrypt.hash("1234", 10);
+    await User.create({
+      name: "Admin",
+      email: "admin@gmail.com",
+      password: hashed,
+      role: "admin"
+    });
+    console.log("Admin created");
+  }
+});
 app.use("/", require("./routes/auth"));
 
 app.listen(4000, () => console.log("Auth service running"));
