a6d0e043eb73f806892abd9a5aebf522c00b9a86
Vadim Kovalyov <vadimkovalyov06@gmail.com>
2026-04-21 12:57:35 +0300

    Updated routes

--- gateway/server.js
+++ gateway/server.js
@@ -8,21 +8,25 @@ app.use(cors());
 app.use("/auth", createProxyMiddleware({
   target: "http://auth-service:4000",
   changeOrigin: true,
+  pathRewrite: { '^/auth': '' },
 }));
 
 app.use("/users", createProxyMiddleware({
   target: "http://user-service:4001",
   changeOrigin: true,
+  pathRewrite: { '^/users': '' },
 }));
 
 app.use("/products", createProxyMiddleware({
   target: "http://product-service:4002",
   changeOrigin: true,
+  pathRewrite: { '^/products': '' },
 }));
 
 app.use("/orders", createProxyMiddleware({
   target: "http://order-service:4003",
   changeOrigin: true,
+  pathRewrite: { '^/orders': '' },
 }));
 
 app.listen(3000, () => console.log("Gateway running"));
